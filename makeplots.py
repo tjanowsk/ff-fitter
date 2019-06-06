b7ab5d23a05f146e0de8dae3a940b61e7c725d07
Tadeusz Janowski <t.janowski@ed.ac.uk>
2019-06-06 13:31:10 +0100

    cleaned up makeplots, added input file support

--- makeplots.py
+++ makeplots.py
@@ -10,45 +10,56 @@ if len(argv) != 4:
     print("num parameters is the number of fit parameters for each form factor")
     exit()
 
+inputfile = argv[1]
 numalpha = int(argv[3])
-
-qsqlist2 = list(range(10,26))
 qsqlist = [0.00001, 1.0, 2.0, 3.0, 5.0, 8.0, 10.0, 12.0, 14.0, 16.0]
-#names = ['Re[P*_d]','Re[Tperp*_d]','Re[Tpar*_d]', 'Im[P*_d]','Im[Tperp*_d]','Im[Tpar*_d]',\
-#    'Re[P*_s]','Re[Tperp*_s]','Re[Tpar*_s]', 'Im[P*_s]','Im[Tperp*_s]','Im[Tpar*_s]']
-names = ['Vperp_u', 'Vpar_u', 'Tperp_u', 'Tpar_u']
-names2 = ['Vperp_d', 'Vpar_d', 'Tperp_d', 'Tpar_d']
-names3 = ['Vperp_s', 'Vpar_s', 'Tperp_s', 'Tpar_s']
 
-polelist_u = (5.325, 5.325, 5.724, 5.724)
-polelist_d = (5.325, 5.325, 5.724, 5.724)
-polelist_s = (5.415, 5.415, 5.829, 5.829)
-filename = argv[1]
+class par:
+    def __init__(self, lb, ub):
+        self.lb = float(lb)
+        self.ub = float(ub)
+        self.ffnames = []
+        self.pole = []
+
+def readinput(filename):
+    d = {}
+    names = []
+    with open(filename,'r') as f:
+        for line in f:
+            if len(line.split()) == 3:
+                name, lb, ub = line.split()
+                names.append(name)
+                d[name] = par(lb,ub)
+            if len(line.split()) == 2:
+                ff, pole = line.split()
+                d[name].ffnames.append(ff)
+                d[name].pole.append(float(pole))
+    return (d, names)
+
+d, names = readinput(inputfile)
+for item in names:
+    print(item, d[item].lb, d[item].ub, d[item].ffnames, d[item].pole)
+
+def getbounds(qsqlist, l, u):
+    for i, it in enumerate(qsqlist):
+        if it>=l:
+            lb = i
+            break
+    for i, it in reversed(list(enumerate(qsqlist))):
+        if it<=u:
+            ub = i+1
+            break
+    return (lb,ub)
 
 alist = list(range(3*numalpha)) + [2*numalpha] + list(range(3*numalpha, 4*numalpha - 1))
+d2 = { item:funfits(item, d[item].ffnames, qsqlist, numalpha=numalpha, alphalist=alist, poles = d[item].pole) for item in d }
 
-fitter = funfits(filename, names, qsqlist, numalpha=numalpha, alphalist=alist, poles = polelist_u)
-lb = 1
-ub = min(10, len(qsqlist))
-fitter.printalphas()
-fitter.genfit(lb,ub,fitform=argv[2])
+shift = 0
+for item in names:
+    d2[item].printalphas(shift)
+    d2[item].genfit(*getbounds(qsqlist, d[item].lb, d[item].ub),fitform=argv[2])
+    d2[item].plot('plots/{}_{}'.format(argv[2],argv[3]))
+    shift += len(d2[item].alphalist)
 
-fitter2 = funfits('../res_Bd_re.h5', names2, qsqlist, numalpha=numalpha, alphalist=alist, poles=polelist_d)
-fitter2.genfit(lb,ub,fitform=argv[2])
-fitter2.printalphas(len(fitter.alphalist))
-
-fitter3 = funfits('../res_Bs_re.h5', names3, qsqlist, numalpha=numalpha, alphalist=alist, poles=polelist_s)
-fitter3.genfit(lb,ub,fitform=argv[2])
-fitter3.printalphas(len(fitter.alphalist)+len(fitter2.alphalist))
-
-np.savetxt('results/alpha_{}_{}'.format(argv[2],argv[3]),np.concatenate((fitter.fit_cv, fitter2.fit_cv, fitter3.fit_cv)))
-np.savetxt('results/cov_{}_{}'.format(argv[2],argv[3]),fitter.covalpha(fitter2,fitter3))
-
-# Plot fits
-
-fitter.plot('plots/{}_{}'.format(argv[2],argv[3]))
-fitter2.plot('plots/{}_{}'.format(argv[2],argv[3]))
-fitter3.plot('plots/{}_{}'.format(argv[2],argv[3]))
-
-if __name__=="__main__":
-    pass
+np.savetxt('results/alpha_{}_{}'.format(argv[2],argv[3]),np.concatenate([d2[item].fit_cv for item in names]))
+np.savetxt('results/cov_{}_{}'.format(argv[2],argv[3]),d2[names[0]].covalpha(*[d2[item] for item in names[1:]]))
